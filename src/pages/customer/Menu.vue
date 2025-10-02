d6ecc5204aac840a2bcd4ca55b2d843e2af8adce
Jetross <jetrossneri07@gmail.com>
2025-10-02 11:54:34 +0800

    redesigned the menu page

--- src/pages/customer/Menu.vue
+++ src/pages/customer/Menu.vue
@@ -111,242 +111,297 @@ onMounted(async () => {
 </script>
 
 <template>
-  <v-container fluid class="pa-0">
-    <!-- Header Section -->
-    <v-card color="primary" class="mb-6">
-      <v-card-text class="text-center py-8">
-        <h1 class="text-h3 font-weight-bold text-white mb-4">Our Menu</h1>
-        <p class="text-h6 text-white opacity-90">
-          Discover our delicious selection of carefully crafted dishes
-        </p>
-      </v-card-text>
-    </v-card>
-
-    <!-- Loading State -->
-    <v-container v-if="loading" class="text-center py-8">
-      <v-progress-circular
-        indeterminate
-        color="primary"
-        size="64"
-        class="mb-4"
-      ></v-progress-circular>
-      <p class="text-h6">Loading menu items...</p>
-    </v-container>
-
-    <!-- Error State -->
-    <v-container v-else-if="error" class="text-center py-8">
-      <v-icon color="error" size="64" class="mb-4">mdi-alert-circle</v-icon>
-      <p class="text-h6 text-error mb-4">{{ error }}</p>
-      <v-btn @click="fetchMenuItems" color="primary" variant="outlined">
-        Try Again
+  <v-app>
+    <!-- Fixed Header -->
+    <v-app-bar elevation="1" color="white" class="px-4 border-b-sm">
+      <v-btn icon variant="text" @click="$router.go(-1)">
+        <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-    </v-container>
 
-    <!-- Menu Content -->
-    <div v-else>
-      <!-- Best Sellers Section -->
-      <v-container class="mb-8">
-        <div class="text-center mb-6">
-          <h2
-            class="text-h4 font-weight-bold mb-2 d-flex align-center justify-center"
-          >
-            <v-icon color="warning" size="32" class="mr-2">mdi-star</v-icon>
-            Best Sellers
-          </h2>
-          <p class="text-subtitle-1 text-grey-darken-1">
-            Our most popular dishes loved by customers
-          </p>
-        </div>
+      <v-spacer />
 
-        <v-row>
-          <v-col
-            v-for="item in bestSellers"
-            :key="item.id"
-            cols="12"
-            sm="6"
-            md="4"
-          >
-            <v-card
-              elevation="4"
-              rounded="xl"
-              class="h-100"
-              style="border: 2px solid #ffd700"
-            >
-              <div style="position: relative">
-                <v-img :src="item.image" :alt="item.name" height="200" cover />
-                <v-chip
-                  color="warning"
-                  size="small"
-                  class="ma-2"
-                  style="position: absolute; top: 0; left: 0"
-                  prepend-icon="mdi-star"
-                  variant="flat"
-                >
-                  Best Seller
-                </v-chip>
-              </div>
+      <div class="text-center">
+        <h2 class="text-h6 font-weight-bold text-grey-darken-3">
+          {{ APP_CONFIG.APP_NAME }}
+        </h2>
+        <p class="text-caption text-grey-darken-1 ma-0">Menu</p>
+      </div>
 
-              <v-card-text>
-                <h3 class="text-h6 font-weight-bold mb-2">{{ item.name }}</h3>
-                <p class="text-body-2 text-grey-darken-1 mb-4">
-                  {{ item.description }}
-                </p>
+      <v-spacer />
 
-                <div class="d-flex justify-space-between align-center">
-                  <div class="text-h6 font-weight-bold text-success">
-                    {{ APP_CONFIG.CURRENCY }}{{ item.price.toFixed(2) }}
-                  </div>
-                  <v-btn
-                    @click="addToCart(item)"
-                    color="primary"
-                    size="small"
-                    rounded="lg"
-                    prepend-icon="mdi-plus"
-                  >
-                    Add
-                  </v-btn>
-                </div>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+      <v-btn icon variant="text">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- Content Area -->
+    <v-main class="grey-lighten-5">
+      <!-- Loading State -->
+      <v-container
+        v-if="loading"
+        class="d-flex flex-column align-center justify-center fill-height"
+      >
+        <v-progress-circular
+          indeterminate
+          color="pink"
+          size="48"
+          class="mb-4"
+        ></v-progress-circular>
+        <p class="text-body-1 text-grey-darken-1">Loading delicious food...</p>
       </v-container>
 
-      <!-- Menu Categories Section -->
-      <v-container>
-        <!-- Category Filter Tabs -->
-        <div class="text-center mb-6">
-          <v-chip-group
-            v-model="selectedCategory"
-            selected-class="text-primary"
-            mandatory
-          >
-            <v-chip
-              v-for="category in categories"
-              :key="category.id"
-              :value="category.id"
-              size="large"
-              class="ma-1"
-              :prepend-icon="category.icon"
-            >
-              {{ category.name }}
-            </v-chip>
-          </v-chip-group>
-        </div>
+      <!-- Error State -->
+      <v-container v-else-if="error" class="text-center pa-8">
+        <v-icon color="error" size="64" class="mb-4">mdi-alert-circle</v-icon>
+        <p class="text-h6 text-error mb-4">{{ error }}</p>
+        <v-btn
+          @click="fetchMenuItems"
+          color="pink"
+          variant="flat"
+          rounded="xl"
+          class="px-6"
+        >
+          Try Again
+        </v-btn>
+      </v-container>
 
-        <!-- Menu Items by Category -->
-        <div v-for="category in categories" :key="category.id">
-          <div
-            v-if="selectedCategory === category.id || selectedCategory === null"
-          >
-            <v-card class="mb-6" elevation="2" rounded="xl">
-              <v-card-text class="text-center py-6">
-                <h2
-                  class="text-h4 font-weight-bold mb-2 d-flex align-center justify-center"
-                >
-                  <v-icon :color="category.color" size="28" class="mr-2">
-                    {{ category.icon }}
-                  </v-icon>
-                  {{ category.name }}
-                </h2>
-                <p class="text-subtitle-1 text-grey-darken-1">
-                  {{ category.description }}
-                </p>
-              </v-card-text>
-            </v-card>
+      <!-- Menu Content -->
+      <div v-else class="pb-16">
+        <!-- Restaurant Info Banner -->
+        <v-card
+          color="orange-darken-2"
+          class="mx-4 mt-4 mb-6"
+          rounded="xl"
+          elevation="2"
+        >
+          <v-card-text class="pa-6">
+            <div class="d-flex align-center mb-3">
+              <v-avatar size="60" class="mr-4">
+                <v-img src="/public/assets/logo1.png" />
+              </v-avatar>
+              <div>
+                <h1 class="text-h5 font-weight-bold text-white mb-1">
+                  {{ APP_CONFIG.APP_NAME }}
+                </h1>
+                <div class="d-flex align-center">
+                  <v-icon color="white" size="16" class="mr-1">mdi-star</v-icon>
+                  <span class="text-white text-body-2 mr-3">4.8</span>
+                  <v-icon color="white" size="16" class="mr-1"
+                    >mdi-clock-outline</v-icon
+                  >
+                  <span class="text-white text-body-2">20-30 min</span>
+                </div>
+              </div>
+            </div>
+            <p class="text-white text-body-2 mb-0">
+              Authentic Filipino cuisine • Fresh ingredients • Pick your meals
+              and check out your order
+            </p>
+          </v-card-text>
+        </v-card>
 
-            <v-row class="mb-8">
-              <v-col
-                v-for="item in getItemsByCategory(category.id)"
-                :key="item.id"
-                cols="12"
-                sm="6"
-                md="4"
+        <!-- Best Sellers Section -->
+        <v-container class="px-4 py-6">
+          <div class="d-flex align-center mb-4">
+            <v-icon color="orange" size="24" class="mr-2">mdi-fire</v-icon>
+            <h2 class="text-h6 font-weight-bold text-grey-darken-3">
+              Most Popular
+            </h2>
+          </div>
+
+          <v-row dense v-if="bestSellers.length > 0">
+            <v-col v-for="item in bestSellers" :key="item.id" cols="12">
+              <!-- FoodPanda-style horizontal item card -->
+              <v-card
+                elevation="1"
+                class="mb-3"
+                @click="addToCart(item)"
+                rounded="xl"
+                hover
               >
-                <v-card elevation="4" rounded="xl" class="h-100">
-                  <div style="position: relative">
-                    <v-img
-                      :src="item.image"
-                      :alt="item.name"
-                      height="200"
-                      cover
-                    />
-                    <v-chip
-                      v-if="item.quantity === 0"
-                      color="red"
-                      size="x-small"
-                      class="ma-2"
-                      style="position: absolute; top: 0; right: 0"
-                      variant="flat"
-                    >
-                      Out of Stock
-                    </v-chip>
+                <v-card-text class="pa-3">
+                  <div class="d-flex">
+                    <div class="flex-grow-1 pr-3">
+                      <h3
+                        class="text-body-1 font-weight-bold text-grey-darken-3 mb-1"
+                      >
+                        {{ item.name }}
+                      </h3>
+                      <p class="text-caption text-grey-darken-1 mb-2">
+                        {{ item.description }}
+                      </p>
+                      <span class="text-h6 font-weight-bold pink--text">
+                        {{ APP_CONFIG.CURRENCY }}{{ item.price.toFixed(2) }}
+                      </span>
+                    </div>
+                    <div class="d-flex flex-column align-center">
+                      <div class="position-relative mb-2">
+                        <v-img
+                          :src="item.image"
+                          :alt="item.name"
+                          width="80"
+                          height="80"
+                          cover
+                          class="rounded-lg"
+                        />
+                        <v-chip
+                          color="orange"
+                          size="x-small"
+                          class="position-absolute"
+                          style="top: -4px; left: -4px"
+                          variant="flat"
+                        >
+                          <v-icon size="12" class="mr-1">mdi-fire</v-icon>
+                          Popular
+                        </v-chip>
+                      </div>
+                      <v-btn
+                        @click.stop="addToCart(item)"
+                        icon
+                        size="small"
+                        color="pink"
+                        variant="flat"
+                      >
+                        <v-icon size="20">mdi-plus</v-icon>
+                      </v-btn>
+                    </div>
                   </div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
 
-                  <v-card-text>
-                    <h3 class="text-h6 font-weight-bold mb-2">
+        <!-- All Menu Items Section -->
+        <v-container class="px-4 pb-6">
+          <div class="d-flex align-center mb-4">
+            <v-icon color="grey-darken-2" size="24" class="mr-2"
+              >mdi-food</v-icon
+            >
+            <h2 class="text-h6 font-weight-bold text-grey-darken-3">
+              All Items
+            </h2>
+          </div>
+
+          <!-- Menu Items List -->
+          <div v-for="item in getItemsByCategory(1)" :key="item.id">
+            <v-card
+              elevation="1"
+              class="mb-3"
+              @click="addToCart(item)"
+              rounded="xl"
+              hover
+              :disabled="item.quantity === 0"
+            >
+              <v-card-text class="pa-3">
+                <div class="d-flex">
+                  <div class="flex-grow-1 pr-3">
+                    <h3
+                      class="text-body-1 font-weight-bold text-grey-darken-3 mb-1"
+                    >
                       {{ item.name }}
                     </h3>
-                    <p class="text-body-2 text-grey-darken-1 mb-3">
+                    <p class="text-caption text-grey-darken-1 mb-2">
                       {{ item.description }}
                     </p>
 
-                    <div v-if="item.quantity > 0" class="mb-3">
+                    <!-- Stock indicator -->
+                    <div
+                      v-if="item.quantity > 0 && item.quantity <= 5"
+                      class="mb-2"
+                    >
                       <v-chip
                         size="x-small"
-                        variant="outlined"
-                        color="success"
-                        class="ma-1"
+                        color="orange"
+                        variant="flat"
+                        class="text-white"
                       >
-                        {{ item.quantity }} available
+                        Only {{ item.quantity }} left
                       </v-chip>
                     </div>
 
-                    <div class="d-flex justify-space-between align-center">
-                      <div class="text-h6 font-weight-bold text-success">
-                        {{ APP_CONFIG.CURRENCY }}{{ item.price.toFixed(2) }}
-                      </div>
-                      <v-btn
-                        @click="addToCart(item)"
-                        color="primary"
-                        size="small"
-                        rounded="lg"
-                        prepend-icon="mdi-plus"
-                        :disabled="item.quantity === 0"
+                    <span
+                      class="text-h6 font-weight-bold pink--text"
+                      :class="item.quantity === 0 ? 'text-grey' : 'pink--text'"
+                    >
+                      {{ APP_CONFIG.CURRENCY }}{{ item.price.toFixed(2) }}
+                    </span>
+                  </div>
+                  <div class="d-flex flex-column align-end">
+                    <div class="position-relative mb-2">
+                      <v-img
+                        :src="item.image"
+                        :alt="item.name"
+                        width="80"
+                        height="80"
+                        cover
+                        class="rounded-lg"
+                      />
+                      <v-overlay
+                        v-if="item.quantity === 0"
+                        contained
+                        class="d-flex align-center justify-center rounded-lg"
                       >
-                        {{ item.quantity === 0 ? "Out of Stock" : "Add" }}
-                      </v-btn>
+                        <span class="text-white text-caption font-weight-bold">
+                          Out of Stock
+                        </span>
+                      </v-overlay>
                     </div>
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
+                    <v-btn
+                      class="mr-3"
+                      v-if="item.quantity > 0"
+                      @click.stop="addToCart(item)"
+                      icon
+                      size="small"
+                      color="pink"
+                      variant="flat"
+                    >
+                      <v-icon size="20">mdi-plus</v-icon>
+                    </v-btn>
+                    <v-btn v-else disabled icon size="small" variant="outlined">
+                      <v-icon size="20">mdi-close</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-card-text>
+            </v-card>
           </div>
-        </div>
-      </v-container>
-    </div>
+        </v-container>
+      </div>
+    </v-main>
 
-    <!-- Cart Summary (Fixed Bottom) -->
-    <v-bottom-navigation v-if="cartItems.length > 0" class="d-flex">
-      <v-container class="d-flex justify-space-between align-center py-2">
-        <div>
-          <div class="text-caption text-grey-darken-1">
-            {{ cartItems.length }} items
-          </div>
-          <div class="text-h6 font-weight-bold">
-            {{ APP_CONFIG.CURRENCY }}{{ cartTotal.toFixed(2) }}
-          </div>
-        </div>
-        <v-btn
-          @click="viewCart"
-          color="success"
-          size="large"
-          rounded="lg"
-          prepend-icon="mdi-cart"
+    <!-- Fixed Bottom Cart -->
+    <v-bottom-navigation
+      v-if="cartItems.length > 0"
+      grow
+      class="px-4"
+      height="80"
+    >
+      <v-btn
+        @click="viewCart"
+        color="pink"
+        variant="flat"
+        block
+        size="large"
+        rounded="xl"
+        class="text-white font-weight-bold ma-2"
+      >
+        <template v-slot:prepend>
+          <v-icon>mdi-cart</v-icon>
+        </template>
+        <span class="mx-2"
+          >{{ cartItems.length }} item{{
+            cartItems.length > 1 ? "s" : ""
+          }}</span
         >
-          View Cart
-        </v-btn>
-      </v-container>
+        <v-spacer />
+        <span class="mr-2"
+          >{{ APP_CONFIG.CURRENCY }}{{ cartTotal.toFixed(2) }}</span
+        >
+        <template v-slot:append>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
+      </v-btn>
     </v-bottom-navigation>
-  </v-container>
+  </v-app>
 </template>
